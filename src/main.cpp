121d57d44a405e50f0dbac05b27a40e2de4dd22e
Igor Faria <iggfaria@gmail.com>
2024-04-23 23:23:10 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,45 @@
-#include <Arduino.h>
 
+//Main Libraries
 #include "M5Cardputer.h"
+#include <string>
 
-void draw_function(LovyanGFX* gfx) {
-    int x      = rand() % gfx->width();
-    int y      = rand() % gfx->height();
-    int r      = (gfx->width() >> 4) + 2;
-    uint16_t c = rand();
-    gfx->fillRect(x - r, y - r, r * 2, r * 2, c);
-}
+//Code Abbreviations
+using namespace std;
+#define lcd M5.Lcd
+
+//Project Libraries
+#include "redOceanIcon.h" //Project logo librarie
+
 
 void setup() {
-    auto cfg = M5.config();
-    M5Cardputer.begin(cfg);
-    int textsize = M5Cardputer.Display.height() / 60;
-    if (textsize == 0) {
-        textsize = 1;
-    }
-    M5Cardputer.Display.setTextSize(textsize);
+    //Screen settings
+    M5.begin();
+    M5.Power.begin();
+    lcd.setRotation(1);
+    lcd.setTextSize(3);
+    lcd.setTextColor(WHITE);
+    lcd.fillScreen(BLACK);
+
+    //Project logo settings
+    lcd.setSwapBytes(true);
+    lcd.setBrightness(255);
+    lcd.pushImage(iconX, iconY, iconWidth, iconHeight, redOceanIcon);
+    delay(2*1000);
 }
 
 void loop() {
-    int x      = rand() % M5Cardputer.Display.width();
-    int y      = rand() % M5Cardputer.Display.height();
-    int r      = (M5Cardputer.Display.width() >> 4) + 2;
-    uint16_t c = rand();
-    M5Cardputer.Display.fillCircle(x, y, r, c);
-    draw_function(&M5Cardputer.Display);
+    /*string msg = "Te amo";
+    
+    lcd.setCursor(0, 0);
+
+    lcd.fillScreen(BLACK);
+    lcd.printf("%s", msg.c_str());
+
+    delay(2*1000);
+    lcd.setCursor(0, 0);
+    msg = "Muitooo";
+    lcd.fillScreen(RED);
+    lcd.printf("%s\n", msg.c_str());
+
+    delay(2*1000);*/
 }
