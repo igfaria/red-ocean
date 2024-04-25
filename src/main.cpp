3e0074860557cb97d458d59c58eefc3c92d240b0
Igor Faria <iggfaria@gmail.com>
2024-04-24 22:35:37 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 //Code Abbreviations
 using namespace std;
 #define lcd M5.Lcd
+//#include <SPI.h>
+//#include <Wire.h>
+#include <Adafruit_GFX.h>
+//#include <Adafruit_SSD1306.h>
 
 //Project Libraries
 #include "redOceanIcon.h" //Project logo librarie
@@ -16,19 +20,41 @@ void setup() {
     M5.begin();
     M5.Power.begin();
     lcd.setRotation(1);
-    lcd.setTextSize(3);
-    lcd.setTextColor(WHITE);
+    lcd.setTextSize(2.5);
+    lcd.setTextColor(0xFFFFF);
     lcd.fillScreen(BLACK);
 
     //Project logo settings
     lcd.setSwapBytes(true);
     lcd.setBrightness(255);
     lcd.pushImage(iconX, iconY, iconWidth, iconHeight, redOceanIcon);
+    delay(5*1000);
+
+    string msg = {" [*] IR\n\n [*] Wifi\n\n [*] Keyboard"};
+    //string msg = " [*] IR";
+    
+    int cursorX = 0;
+    int cursorY = 20;
+    int targetX = 0;
+    int targetY = 15;
+    int i = 0;
+
+    lcd.setCursor(cursorX, cursorY);
+    lcd.fillScreen(BLACK);
+    lcd.fillRect(targetX, targetY, iconWidth, 30, RED);
+    //lcd.setTextColor(0xFFFFF);
+    lcd.printf("%s", msg.c_str());
+    //lcd.printf("%-15s", msg.c_str());
+    cursorY += 20;
     delay(2*1000);
 }
 
+
+
+
 void loop() {
-    /*string msg = "Te amo";
+    /*=
+    string msg = "Te amo";
     
     lcd.setCursor(0, 0);
 
@@ -41,5 +67,6 @@ void loop() {
     lcd.fillScreen(RED);
     lcd.printf("%s\n", msg.c_str());
 
-    delay(2*1000);*/
+    delay(2*1000);
+    */
 }
