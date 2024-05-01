8622ab01b92f1abf8b17cec3ec5c97404059302b
Igor Faria <iggfaria@gmail.com>
2024-04-30 23:32:59 -0300

    Create menuOptions.h

---
+++ src/menuOptions.h
@@ -0,0 +1,9 @@
+#ifndef MENUOPTIONS_H
+#define MENUOPTIONS_H
+
+// Main and sub menus
+string mainOptions [] = {"[*] IR", 
+                         "[*] Wifi", 
+                         "[*] Keyboard"};
+
+#endif /*MENUOPTIONS_H*/
