f2ec1434bc261c682eac792fbf2916ce84ed7a3d
Igor Faria <iggfaria@gmail.com>
2024-04-30 23:15:02 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,67 +6,59 @@
 //Code Abbreviations
 using namespace std;
 #define lcd M5.Lcd
-//#include <SPI.h>
-//#include <Wire.h>
-#include <Adafruit_GFX.h>
-//#include <Adafruit_SSD1306.h>
+
 
 //Project Libraries
 #include "redOceanIcon.h" //Project logo librarie
+#include "menu.h"
+
+//Screen definitions
+#define screenColor 0x0000
+#define screenRotation 1
+#define screenWidth 240
+#define screenHeight 135
+
+//Font definitions
+#define fontColor 0xFFFF
+#define fontSmall 1.5
+#define fontMedium 2.5
+#define fontHigh 3
+
+//Rect definitions
+#define rectColor 0xF000
+#define rectX 0
+#define rectY 13 //Somar de 40 em 40
+#define rectHeight 30
+
+//Cursor definitions
+#define cursorX 0
+#define cursorY 20
+
+// Main and sub menus
+string menuOptions [] = {"[*] IR", 
+                         "[*] Wifi", 
+                         "[*] Keyboard"};
 
 
 void setup() {
     //Screen settings
     M5.begin();
     M5.Power.begin();
-    lcd.setRotation(1);
-    lcd.setTextSize(2.5);
-    lcd.setTextColor(0xFFFFF);
-    lcd.fillScreen(BLACK);
+    lcd.setRotation(screenRotation);
+    lcd.setTextSize(fontMedium);
+    lcd.setTextColor(fontColor);
+    lcd.fillScreen(screenColor);
 
     //Project logo settings
     lcd.setSwapBytes(true);
-    lcd.setBrightness(255);
+    lcd.setBrightness(60);
     lcd.pushImage(iconX, iconY, iconWidth, iconHeight, redOceanIcon);
     delay(5*1000);
 
-    string msg = {" [*] IR\n\n [*] Wifi\n\n [*] Keyboard"};
-    //string msg = " [*] IR";
-    
-    int cursorX = 0;
-    int cursorY = 20;
-    int targetX = 0;
-    int targetY = 15;
-    int i = 0;
-
-    lcd.setCursor(cursorX, cursorY);
-    lcd.fillScreen(BLACK);
-    lcd.fillRect(targetX, targetY, iconWidth, 30, RED);
-    //lcd.setTextColor(0xFFFFF);
-    lcd.printf("%s", msg.c_str());
-    //lcd.printf("%-15s", msg.c_str());
-    cursorY += 20;
-    delay(2*1000);
+    drawMenu(menuOptions, sizeof(menuOptions)/sizeof(menuOptions[0]));
 }
 
 
-
-
 void loop() {
-    /*=
-    string msg = "Te amo";
-    
-    lcd.setCursor(0, 0);
 
-    lcd.fillScreen(BLACK);
-    lcd.printf("%s", msg.c_str());
-
-    delay(2*1000);
-    lcd.setCursor(0, 0);
-    msg = "Muitooo";
-    lcd.fillScreen(RED);
-    lcd.printf("%s\n", msg.c_str());
-
-    delay(2*1000);
-    */
 }
