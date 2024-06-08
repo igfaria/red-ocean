b7f8548a5cf78b8b8fa2559caafe4fa1dc3ce78d
Igor Faria <iggfaria@gmail.com>
2024-06-08 14:44:00 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 //Main Libraries
 #include "M5Cardputer.h"
-#include <string>
+//#include <string>
 
 //Code Abbreviations
 using namespace std;
@@ -14,6 +14,8 @@ using namespace std;
 #include "menuOptions.h"
 #include "menu.h"
 #include "menuControls.h"
+//#include "keyboardConfig.h"
+
 
 //Screen definitions
 #define screenColor 0x0000
@@ -41,8 +43,17 @@ int optionPosition = 1;
 #define cursorX 0
 #define cursorY 20
 
-void setup() {
-    //Screen settings
+//Options size
+#define mainMenuSize sizeof(mainOptions)/sizeof(mainOptions[0])
+//#define keyboardSize sizeof(keyboardOptions)/sizeof(keyboardOptions[0])
+int size = mainMenuSize;
+String *options = mainOptions;
+
+int applicationOption = 0;
+int applicationLink = 0;
+
+void screenSettings(){
+    lcd.clear();
     auto cfg = M5.config();
     M5Cardputer.begin(cfg, true);
     M5.Power.begin();
@@ -51,11 +62,17 @@ void setup() {
     lcd.setTextSize(fontMedium);
     lcd.setTextColor(fontColor);
     lcd.fillScreen(screenColor);
+    lcd.setTextDatum(top_left);
+}
+
+void setup() {
+    //Screen settings
+    screenSettings();
 
     redOceanIcon(); //Project logo function
 
     //drawMenu(mainOptions, sizeof(mainOptions)/sizeof(mainOptions[0]), rectY, optionPosition);
-    rectPosition(rectSum, sizeof(mainOptions)/sizeof(mainOptions[0]), mainOptions, optionPosition);
+    rectPosition(rectSum, size, options, optionPosition);
 }
 
 void loop() {
@@ -63,17 +80,42 @@ void loop() {
     M5Cardputer.update();
     if (M5Cardputer.Keyboard.isChange()){
         if (M5Cardputer.Keyboard.isKeyPressed('.')){  
-            optionPosition = arrowDown(rectSum, sizeof(mainOptions)/sizeof(mainOptions[0]), optionPosition);
-            rectSum = rectPosition(rectSum, sizeof(mainOptions)/sizeof(mainOptions[0]), mainOptions, optionPosition);
+            optionPosition = arrowDown(rectSum, size, optionPosition);
+            rectSum = rectPosition(rectSum, size, options, optionPosition);
         }
 
         if (M5Cardputer.Keyboard.isKeyPressed(';')){
-            optionPosition = arrowUp(rectSum, sizeof(mainOptions)/sizeof(mainOptions[0]), optionPosition);
-            rectSum = rectPosition(rectSum, sizeof(mainOptions)/sizeof(mainOptions[0]), mainOptions, optionPosition);
+            optionPosition = arrowUp(rectSum, size, optionPosition);
+            rectSum = rectPosition(rectSum, size, options, optionPosition);
         }
             
-        if (M5Cardputer.Keyboard.isKeyPressed(KEY_ENTER)){
-            drawMenu(mainOptions, sizeof(mainOptions)/sizeof(mainOptions[0]), rectY, 1);
+        if (M5Cardputer.Keyboard.isKeyPressed(KEY_ENTER) && applicationOption == 0){
+            /* Enter action
+            if(optionPosition==3){
+                options = keyboardOptions;
+                size = keyboardSize;
+                applicationLink = optionPosition;
+                optionPosition = 1;
+                rectSum = rectY;
+                applicationOption = 1;
+                rectSum = rectPosition(rectSum, size, options, optionPosition);
+            }*/
+        }else
+        if(M5Cardputer.Keyboard.isKeyPressed(KEY_ENTER) && applicationOption == 1){
+            //Option Configuration
+            //if(applicationLink == 3 && optionPosition == 1) runUSBKeyboard();
+            //if(applicationLink == 3 && optionPosition == 2) runBleKeyboard();
+        }
+
+        if(M5Cardputer.Keyboard.isKeyPressed(KEY_OPT)){
+            options = mainOptions;
+            size = mainMenuSize;
+            optionPosition = 1;
+            rectSum = rectY;
+            applicationOption = 0;
+            applicationLink = 0;
+            screenSettings();
+            rectPosition(rectSum, size, options, optionPosition);
         }
     }
 }
